bce6c382f3ce441e7299abab2b26fd81805a2a90
Sidney Wilcox <sidney.d.wilcox@gmail.com>
2026-08-16 08:15:28 -0400

    Fix missing quotes on Stripe URL in page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -713,7 +713,7 @@ export default function Home() {
             </p>
 
             <a
-              href={https://buy.stripe.com/bJe6oAaB4alO1ne9DJefC04}
+              href={"https://buy.stripe.com/bJe6oAaB4alO1ne9DJefC04"}
               style={{
                 display: "block",
                 width: "100%",
